d93ce4637b457cf95c915f9a59d8aa42fbd0428d
Mesel <meselisaac03@hotmail.com>
2019-03-22 17:59:49 +0000

    created function to slide the new-tweet section up and down when compose button is clicked

---
+++ public/scripts/compose-button.js
@@ -0,0 +1,29 @@
+$(function() {
+
+  $("#compose").on("click", function(){
+  ($(".new-tweet").slideToggle())
+    $("textarea").focus();
+  // }
+  //   $(".new-tweet").slideToggle();
+
+  })
+
+
+
+
+  })
+
+
+
+  // $("textarea").on("input", charCounter);
+
+
+  // $("#compose").click(function() {
+  //   $(".new-tweet").slideUp();
+
+  // })
+
+  // $("#compose").click(function() {
+  //   $(".new-tweet").slideDown();
+  //   $("textarea").focus();
+  // })
