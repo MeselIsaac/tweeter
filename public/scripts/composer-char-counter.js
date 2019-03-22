99af31892fd56256ffc12bcad37fbebff3dc1e7e
Mesel <meselisaac03@hotmail.com>
2019-03-22 18:12:43 +0000

    modified character counter code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,17 +2,27 @@ $(function() {
 
   function charCounter (event) {
     let num = 140;
-    let countdown = num - event.target.value.length;
+    let countdown = num - $(this).val().length;
     if (countdown < 0) {
-      $(".counter").text(countdown).css('color', 'red');
+      $(".counter").text(countdown).css("color", "red");
     } else if (countdown >= 0) {
-      $(".counter").text(countdown).css('color', '#244751');
+      $(".counter").text(countdown).css("color", "#244751");
     }
   }
 
-  $("textarea").on('input', charCounter);
+  $("textarea").on("input", charCounter);
 
+  // function hoverIn() {
+  //   $(this).css("opacity", )
+  // }
 
+  // function hoverOut() {
+  //   $(this).css("background-color", "blue")
+  // }
+
+  // $("#tweet-container").hover(hoverIn);
 
 });
+
+
 
