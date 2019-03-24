cd7998b4a86dd667074e352da71a291c8e23dc73
Mesel <meselisaac03@hotmail.com>
2019-03-24 00:13:51 +0000

    cleaned up code

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,42 +9,20 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      // var result = []
-      console.log(newTweet);
-        db.collection("tweets").insertOne( newTweet ,(err, newTweet) =>{
-          if (err) throw err;
-          console.log("posting tweet")
-          callback(null, newTweet);
-        });
-      },
-
-
+      db.collection("tweets").insertOne( newTweet ,(err, newTweet) =>{
+        if (err) throw err;
+        callback(null, newTweet);
+      });
+    },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
       db.collection("tweets").find().toArray((err, tweets) => {
         if (err) throw err;
-        console.log("Logging each tweet:");
         callback(null, tweets)
       });
     }
-
   };
 }
 
 
-   // , function(err, r) {
-   //        assert.equal(null, err);
-   //        assert.equal(1, r.insertedCount);
-
-
-   // saveTweet: function(newTweet, callback) {
-   //   // console.log(newTweet)
-   //   var result = []
-   //   db.collection('tweets').insertOne(newTweet).then((result) => {
-   //     callback('ok',result);
-   //   })
-   //   .catch((err) => {
-   //     callback('err',err);
-   //   })
-   // },
