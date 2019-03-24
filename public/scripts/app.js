a62bfa7353e96168012b098074f6fc6ccdd93ecc
Mesel <meselisaac03@hotmail.com>
2019-03-24 00:25:17 +0000

    texter counter restarts after tweet is send and code is cleaned up

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,72 +6,62 @@
 //
 $(function() {
 
+  function createTweetElement(tweet) {
+    let $tweets = $("<article class='tweets'>");
+    let $img = $("<img class='icon' src="+tweet.user.avatars.regular+">");
+    let $h2 = $("<h2>").text(tweet.user.name);
+    let $h3 = $("<h3>").text(tweet.user.handle);
+    let $div = $("<div>").text(tweet.content.text);
+    let $p2 = $("<p>").text(tweet.created_at);
+    let $img1= $("<img class='retweet' src='/images/retweet.png'>");
+    let $header = $("<header>").append($img).append($h2).append($h3).append($div);
+    let $footer = $("<footer>").append($p2).append($img1);
 
-function createTweetElement(tweet) {
-  let $tweets = $("<article class='tweets'>")
+    $tweets.append($header).append($footer);
 
-  let $img = $("<img class='icon' src="+tweet.user.avatars.regular+">");
-  let $h2 = $("<h2>").text(tweet.user.name);
-  let $h3 = $("<h3>").text(tweet.user.handle);
-  let $div = $("<div>").text(tweet.content.text);
-  let $p2 = $("<p>").text(tweet.created_at);
-  let $img1= $("<img class='retweet' src='/images/retweet.png'>")
-
-  let $header = $("<header>").append($img).append($h2).append($h3).append($div);
-  let $footer = $("<footer>").append($p2).append($img1)
-
-  $tweets.append($header).append($footer);
-
-  return $tweets;
-}
-
-
-
-
-function renderTweets(array) {
-  for (tweet of array) {
-    $("#tweet-container").prepend((createTweetElement(tweet)))
-
+    return $tweets;
   }
 
-}
+  function renderTweets(array) {
+    for (tweet of array) {
+      $("#tweet-container").prepend((createTweetElement(tweet)));
+    }
+  }
 
+  function loadTweets() {
+    $.ajax("/tweets", {method: "GET"})
+    .then(function(arrayOfTweets) {
+      renderTweets(arrayOfTweets);
+    })
+  }
 
-function loadTweets() {
-  $.ajax("/tweets", {method: "GET"})
-  .then(function(arrayOfTweets) {
-    renderTweets(arrayOfTweets);
+  loadTweets()
+
+  $("form").submit(function(event) {
+    event.preventDefault();
   })
 
-}
-
-loadTweets()
-
-$("form").submit(function(event) {
-        event.preventDefault()
+  var form = $("form");
+  form.on("submit", function() {
+    if (!$("textarea").val()) {
+      alert("Please compose message before submitting your tweet");
+    } else if ($("textarea").val().length > 140) {
+      alert(`Exceeded character limit by ${$("textarea").val().length - 140}`)
+    } else {
+      $.ajax({
+        url: "/tweets",
+        type: "POST",
+        data: $("textarea").serialize()
       })
-
-var form = $("form");
-form.on("submit", function() {
-  if (!$("textarea").val()) {
-    alert("Please compose message before submitting your tweet");
-  } else if ($("textarea").val().length > 140) {
-    alert(`Exceeded character limit by ${$("textarea").val().length - 140}`)
-  } else {
-    $.ajax({
-      url: "/tweets",
-      type: "POST",
-      data: $("textarea").serialize()
-    })
-    .then($.ajax("/tweets", {method: "GET" }).then(function(displayTweets){
-      $("#tweet-container").children().remove();
-      loadTweets();
-       $("textarea").val('');
-       $(".counter").text(140);
-
-    }))
-  }
-})
+      .then($.ajax("/tweets", {method: "GET" }).then(function(displayTweets){
+        $("#tweet-container").children().remove();
+        loadTweets();
+        $("textarea").val('');
+        $(".counter").text(140);
+        return false;
+      }))
+    }
+  })
 })
 
 
